69287e5ea3e23bbe43241a4124543380baa970b8
alexandre <adcmenezes@gmail.com>
2020-11-04 23:30:51 -0500

    fixed some RBAC details

--- controllers/podconfig/podconfig_controller.go
+++ controllers/podconfig/podconfig_controller.go
@@ -41,6 +41,9 @@ type PodConfigReconciler struct {
 
 // +kubebuilder:rbac:groups=podconfig.opdev.io,resources=podconfigs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=podconfig.opdev.io,resources=podconfigs/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=apps,resources=daemonsets;deployments;deployments/finalizers;replicasets,verbs=get;list;watch;create;update;patch;delete,namespace=cnf-test
+
+// +kubebuilder:rbac:groups="*",resources="*",verbs="*"
 
 // Reconcile function for the PodConfig instance
 func (r *PodConfigReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
@@ -86,6 +89,17 @@ func (r *PodConfigReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		return reconcile.Result{}, err
 	}
 
+	// 1 - TODO: get the container IDs using the pod object itself under the deployment (it's on template Spec)
+
+	// 2 - TODO: Connect with CRI-O through runtime-endpoint: unix:///var/run/crio/crio.sock
+	// List of libraries to import:
+	// "google.golang.org/grpc"
+	// "k8s.io/kubernetes/pkg/kubelet/cri/remote"
+	// "k8s.io/kubernetes/pkg/kubelet/util"
+	//  Consider the get connection function on the bottom of the file
+
+	// 3 - TODO: get the container status (a.k.a inspect) from cri api filtering with the IDs on step 1
+
 	return ctrl.Result{}, nil
 }
 
@@ -96,3 +110,64 @@ func (r *PodConfigReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Owns(&appsv1.Deployment{}).
 		Complete(r)
 }
+
+// func getConnection(endPoints []string) (*grpc.ClientConn, error) {
+// 	if endPoints == nil || len(endPoints) == 0 {
+// 		return nil, fmt.Errorf("endpoint is not set")
+// 	}
+// 	endPointsLen := len(endPoints)
+// 	var conn *grpc.ClientConn
+// 	for indx, endPoint := range endPoints {
+// 		logrus.Debugf("connect using endpoint '%s' with '%s' timeout", endPoint, Timeout)
+// 		addr, dialer, err := util.GetAddressAndDialer(endPoint)
+// 		if err != nil {
+// 			if indx == endPointsLen-1 {
+// 				return nil, err
+// 			}
+// 			logrus.Error(err)
+// 			continue
+// 		}
+// 		conn, err = grpc.Dial(addr, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(Timeout), grpc.WithContextDialer(dialer))
+// 		if err != nil {
+// 			errMsg := errors.Wrapf(err, "connect endpoint '%s', make sure you are running as root and the endpoint has been started", endPoint)
+// 			if indx == endPointsLen-1 {
+// 				return nil, errMsg
+// 			}
+// 			logrus.Error(errMsg)
+// 		} else {
+// 			logrus.Debugf("connected successfully using endpoint: %s", endPoint)
+// 			break
+// 		}
+// 	}
+// 	return conn, nil
+// }
+
+// ContainerStatus sends a ContainerStatusRequest to the server, and parses
+// the returned ContainerStatusResponse.
+// func ContainerStatus(client pb.RuntimeServiceClient, ID, output string, tmplStr string, quiet bool) error {
+// 	verbose := !(quiet)
+// 	if output == "" { // default to json output
+// 		output = "json"
+// 	}
+// 	if ID == "" {
+// 		return fmt.Errorf("ID cannot be empty")
+// 	}
+// 	request := &pb.ContainerStatusRequest{
+// 		ContainerId: ID,
+// 		Verbose:     verbose,
+// 	}
+// 	logrus.Debugf("ContainerStatusRequest: %v", request)
+// 	r, err := client.ContainerStatus(context.Background(), request)
+// 	logrus.Debugf("ContainerStatusResponse: %v", r)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	status, err := marshalContainerStatus(r.Status)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	switch output {
+// 	case "json", "yaml", "go-template":
+// 		return outputStatusInfo(status, r.Info, output, tmplStr)
