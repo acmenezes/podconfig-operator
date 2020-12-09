fc0f3a2f72fcded2229094dcad9b21f94fca8e27
acmenezes <adcmenezes@gmail.com>
2020-12-09 16:54:55 +0000

    addind trunk poort kind

--- main.go
+++ main.go
@@ -76,6 +76,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "PodConfig")
 		os.Exit(1)
 	}
+	if err = (&podconfigcontroller.TrunkPortReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("TrunkPort"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "TrunkPort")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
